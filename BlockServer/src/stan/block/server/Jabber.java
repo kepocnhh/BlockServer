7626e754154abde44c74c76468a88892e5e661ed
kepocnhh <kepocnhh@gmail.com>
2014-11-25 06:41:08 +0400

    Переставляю проекты

--- BlockServer/src/stan/block/server/Jabber.java
+++ BlockServer/src/stan/block/server/Jabber.java
@@ -13,7 +13,7 @@ import java.util.List;
 import stan.*;
 
 public class Jabber
-                            extends Thread
+        extends Thread
 {
 //Поля////////////////////////////////////////////////////////////////////////////////////
     private Socket socket;
